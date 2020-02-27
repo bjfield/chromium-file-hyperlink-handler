c86f171ada77be5643fb3bfefe28ca8938156bef
Benjamin Field <Ben.Field@chelanpud.org>
2020-02-27 08:19:22 -0800

    Add code explainer comments to content.js.

--- scripts/content.js
+++ scripts/content.js
@@ -59,19 +59,24 @@ window.addEventListener('click', event => {
 
       log("content.js : allowing", url);
 
-      // Don't follow through with the hyperlink event.
+      // Stop the event from processing and from being propogated to other
+      // target elements (e.g., the anchor element itself).
       event.preventDefault();
       
       // Instead, the background script will open a new tab.
 			try {
+
         // Send a message to be received by the background script.
 				chrome.runtime.sendMessage({
 					method: 'openLocalFile',
-					localFileUrl: url,
-				});
+          localFileUrl: url,
+          
+        });
+        
         log("content.js : openLocalFile message sent");
+
 			} catch (e) {
-        // Catch any error that trigger when the extension is reloaded.
+        // Catch any errors that trigger when the extension is loaded.
         log("content.js : error sending openLocalFile message", e);
       }
 
@@ -79,6 +84,12 @@ window.addEventListener('click', event => {
     
 	}
 }, {
-  // Own the click event until we are done with it.
+  // Please allow the listener object (window) to handle this event before it
+  // is dispatched to the target(s) of the click event. And dispatch it in a
+  // "trickle down" fashion from first parent to last child, not "bubble up"
+  // from last child to first parent. In conjunction with the call to
+  // event.preventDefault() in the handler above, this means no target element
+  // will receive the event. It will stop with this listener, which is the
+  // top-most object in the tree.
 	capture: true,
 });
